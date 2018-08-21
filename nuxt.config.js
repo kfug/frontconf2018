4e0c85e91c30cdcd16d9dd2b55ab99b653c8351f
t.goto <t.goto@chatbox-inc.com>
2018-08-21 11:55:21 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'frontconf2018',
+    title: 'FRONTEND CONFERENCE 2018',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Webで働く全ての人のために、FRONTEND CONFERENCE 2017年 11月24日 大阪' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
