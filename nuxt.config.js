d750bb488752440e52a3ebc0d346c53210d4edfa
t.goto <t.goto@chatbox-inc.com>
2018-08-21 11:42:42 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css:[
+    "~/assets/scss/app.scss"
+  ],
   /*
   ** Customize the progress bar color
   */
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,26 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        2018 Nov
+      <h1>
+        FRONTEND <br>
+        CONFERENCE <br>
+        2018
       </h1>
-      <h2 class="subtitle">
-        FRONTEND CONFERENCE 2018
-      </h2>
-      <div class="links">
-        <a
-          href=""
-          target="_blank"
-          class="button--green">Join Slack</a>
-        <a
-          href=""
-          target="_blank"
-          class="button--orange">GitHub</a>
 
-        <p class="prevLinks" style="margin: 1em">
-          <a href="http://kfug.jp/frontconf2016/">2016</a>
-          &nbsp;
-          <a href="http://kfug.jp/frontconf2017/">2017</a>
-        </p>
+      <p class="time">
+        2018.11.24
+      </p>
+      <p class="place">
+        @Osaka
+      </p>
+      <p class="message">
+        デザイナからエンジニア、 <br>
+        Webに携わる全ての人へ
+      </p>
+
+      <div class="logo">
+        powored by <br>
+        <img style="margin-top:5px;width: 150px" src="/kfug_logo.png" alt="">
       </div>
     </div>
   </section>
@@ -38,44 +36,24 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped>
+  *{
+    text-align: center;
+    color: #555;
+  }
+  h1{
+    margin-top: 4rem;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    letter-spacing: 2px;
+  }
+  .time,.place{
+    margin-top: 2rem;
+    font-size: 1.3rem;
+  }
+  .message{
+    margin-top: 2rem;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 60px;
-  line-height: 80px;
-  color: #35495e;
-  letter-spacing: 1px;
-  margin-bottom: 20px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-
-.prevLinks a{
-  color: #26ACE0;
-  font-weight: bold;
-}
-.prevLinks a:visited{
-  color: #26ACE0;
-}
 </style>
 
