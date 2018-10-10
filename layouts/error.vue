76dfa581366ca8085983ef93a5eb5b03a68a636a
ririko <ririkokanikama25@yahoo.co.jp>
2018-10-10 17:58:23 +0900

    404ページのコーディング (#19)

---
+++ layouts/error.vue
@@ -0,0 +1,70 @@
+<template>
+    <section>
+        <div class="p-error">
+            <div class="text">
+                <div class="text_title">
+                <h1 v-if="error.statusCode === 404">404　Not Found</h1>
+                <h1 v-else>エラーが発生しました</h1>
+                </div>
+                <p class="text_description">
+                    お探しのページが見つかりませんでした。
+                </p>
+                <div class="link">
+                <a href="https://2018.kfug.jp/">フロントエンドカンファレンス2018サイトへ</a>
+            </div>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: ['error'],
+        layout: 'errorLayout',
+    }
+</script>
+
+
+<style lang="scss" scoped>
+    @import "~/assets/scss/_library.scss";
+
+    .p-error {
+        min-height: 100vh;
+        text-align: center;
+        padding: 30px 0 60px;
+        color: white;
+
+        //エラーの内容
+        .text {
+            padding: 40px 0 ;
+            text-align: center;
+            color: white;
+            @include desktop {
+                padding: 90px 0;
+            }
+
+            &_title {
+                @include c-title;
+            }
+            &_description {
+                margin: 0 auto;
+                max-width: 680px;
+            }
+        }
+    }
+    h1{
+        font-size:5rem;
+    }
+    .link{
+        margin-top:50px;
+    }
+    a{
+        text-decoration: none;
+        color: #353D60;
+    }
+
+
+
+</style>
+
---
+++ layouts/errorLayout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="l-bgWrap">
+        <div class="l-bg">
+            <nuxt/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['error'],
+        layout: 'error',
+    }
+</script>
+
+
+<style lang="scss" scoped>
+    @import "~/assets/scss/_library.scss";
+
+    .l-bgWrap {
+        min-height: 100%;
+        background: linear-gradient(135deg, #7171c7 0%,#de6d62 100%);
+        background-size: cover;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+    .l-bg {
+        background-image:
+                url(~assets/images/bg_pattern.svg),
+                url(~assets/images/bg_texture.png);
+        background-size: 30%;
+    }
+</style>
+
