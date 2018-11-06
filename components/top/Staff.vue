9e7c703fc07953410c987e16202ef0bc86aa254c
yasui05821 <r.yasui@chatbox-inc.com>
2018-11-06 18:21:56 +0900

    「access」「staff」用 (#26)
    * アクセス用の画像追加
    
    * Access、Staffのコンポーネント準備

---
+++ components/top/Staff.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <div class="p-staff">
+      <div class="p-container">
+        <div class="intro">
+          <h2 class="c-title">
+            STAFF
+          </h2>
+          <p class="description">
+            仮テキスト
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  @import "~/assets/scss/_library.scss";
+
+  .p-staff {
+    //イントロ
+    .intro {
+      padding: 40px 0;
+      text-align: center;
+      color: white;
+      @include desktop {
+        padding: 90px 0;
+      }
+
+      .c-title {
+        @include c-title;
+      }
+      .description {
+        margin: 0 auto;
+        max-width: 680px;
+      }
+    }
+
+  }
+
+</style>
+
