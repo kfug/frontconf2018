79a5c6f6120e733c71e9a630e7086c4a5a8bd526
yasui05821 <yasui05821@gmail.com>
2018-11-06 12:30:32 +0900

    Access、Staffのコンポーネント準備

---
+++ components/top/Access.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="p-access">
+    <div class="c-container">
+      <img class="circle--outline" src="~/static/images/parts_circle-outline.svg">
+      <img class="line" src="~/static/images/parts_line.svg">
+      <div class="c-block">
+        <h3 class="title">
+          ACCESS
+        </h3>
+        <p class="description">
+          マークアップよろしくです！
+        </p>
+        <router-link to="/session" class="c-btn">
+          <span>セッション一覧を見る</span>
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  @import "~/assets/scss/_library.scss";
+
+  .p-access {
+    @include p-padding;
+
+    .c-btn {
+      @include c-btn--white;
+    }
+
+    .c-container {
+      text-align: right;
+      position: relative;
+    }
+    .circle--outline {
+      max-width: 50px;
+      position: absolute;
+      top: -70px;
+      left: 200px;
+      @include desktop {
+        max-width: 60px;
+      }
+    }
+    .line {
+      max-width: 160px;
+      transform: rotate(73deg);
+      position: absolute;
+      top: -80px;
+      left: 150px;
+      @include desktop {
+        max-width: 300px;
+        top: -180px;
+        left: 60px;
+      }
+    }
+
+    @include c-block--outline;
+  }
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,9 @@
       <p-about></p-about>
       <p-session></p-session>
       <p-sponsor></p-sponsor>
+      <p-access></p-access>
       <p-sponsorlist/>
+      <p-staff/>
     </main>
 </template>
 
@@ -13,7 +15,9 @@ import PHero from '~/components/top/Hero.vue'
 import PAbout from '~/components/top/About.vue'
 import PSession from '~/components/top/Session.vue'
 import PSponsor from '~/components/top/Sponsor.vue'
+import PAccess from '~/components/top/Access.vue'
 import PSponsorlist from './-sponsor.vue'
+import PStaff from '~/components/top/Staff.vue'
 
 export default {
   components: {
@@ -21,13 +25,16 @@ export default {
     PAbout,
     PSession,
     PSponsor,
-    PSponsorlist
+    PAccess,
+    PSponsorlist,
+    PStaff
   }
 }
 </script>
 
 <style lang="scss" scoped>
   @import "~/assets/scss/_library.scss";
+
 
 </style>
 
